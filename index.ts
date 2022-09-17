8e255a20c0f703b5869619c5eee8119645979298
jdcheek <58921502+jdcheek@users.noreply.github.com>
2022-09-16 21:37:45 -0500

    project setup and basic types

---
+++ index.ts
@@ -0,0 +1,10 @@
+// Basic types in javascript
+const isOpen: boolean = false;
+// const name: string = 'ricky'; // name is defined on the window object by typescript
+const myAge: number = 33;
+const list: number[] = [1, 2, 3];
+const me: [string, number, boolean] = ['steve', 32, false];
+enum = Job { webdev, webdesigner, prime minister};
+const job: Job = Job.webdev;
+const phone: any = 'pixel';
+const tablet: any = 3;
