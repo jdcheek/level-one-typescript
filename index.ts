375f20b513fd82f2cbd1779b7db9b8c3738a8ccc
jdcheek <58921502+jdcheek@users.noreply.github.com>
2022-09-17 21:05:25 -0500

    functions, opt def and rest params, implicit types, union types

--- index.ts
+++ index.ts
@@ -8,3 +8,47 @@ enum Job { webdev, webdesigner, prime minister};
 const job: Job = Job.webdev;
 const phone: any = 'pixel';
 const tablet: any = 3;
+
+// functions in typescript
+
+// params and returns explicit types
+// ? for optional params
+const sayWord = (word?: string): string => {
+  console.log(word);
+  return word; // must be string
+};
+
+const printNum = (num = 5, ...otherNumbers: number[]): number => {
+  console.log(otherNumbers);
+  return num; // must be number
+};
+
+sayWord('dave');
+printNum(3, [2, 5, 8]);
+
+// Implicit types
+
+// typescript infers type on instanciation and will error when type is changed, use :any to override but probably shouldn't
+let newName = 'mark';
+newName = 'bill'; 
+newName = 19;
+
+// type is inferred from other variables as well when instanciation not when changed 
+let newNameTwo = newName;
+newNameTwo = 19;
+
+// Union types with | 
+ const makeMargin = (x: string | number): string => {
+   return `margin: ${x}px;`;
+ };
+  
+ makeMargin(10);
+ makeMargin('10');
+ makeMargin(false);
+
+ // Null types
+ // null is included
+ let dog: string | undefined = 'sal';
+ dog = null;
+ dog = 'bo';
+ dog = undefined;
