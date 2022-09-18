c92cd75b7da40a1215641211527df8f66a19d4a1
jdcheek <58921502+jdcheek@users.noreply.github.com>
2022-09-17 19:56:50 -0500

    typo: removed assign operator from enum causing error

--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@ const isOpen: boolean = false;
 const myAge: number = 33;
 const list: number[] = [1, 2, 3];
 const me: [string, number, boolean] = ['steve', 32, false];
-enum = Job { webdev, webdesigner, prime minister};
+enum Job { webdev, webdesigner, prime minister};
 const job: Job = Job.webdev;
 const phone: any = 'pixel';
 const tablet: any = 3;
